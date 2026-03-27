ef039f66ca1c4d7c5746e241d365dccb0c1f999e
TerryChie <73026499+TerryChie@users.noreply.github.com>
2026-03-27 23:11:23 +0500

    style changes, added fonts and images
    colors, borders, bgLogo, sidebar blur, some paddings and margins

--- js/components.js
+++ js/components.js
@@ -17,8 +17,6 @@ async function loadLayout() {
                 const response = await fetch(item.url);
                 if (response.ok) {
                     target.innerHTML = await response.text();
-                    // Здесь можно вызвать функцию инициализации меню, 
-                    // если оно находится внутри хедера
                     initNavigation();
                 }
             } catch (err) {
@@ -30,27 +28,19 @@ async function loadLayout() {
 }
 
 function initNavigation() {
-    // 1. Получаем путь текущей страницы (например, "/index.html" или "/labs.html")
-    // Если мы на главной (просто домен), pathname будет "/"
     const currentPath = window.location.pathname;
 
-    // 2. Находим все ссылки в навигации
     const navLinks = document.querySelectorAll('.nav-button');
 
     navLinks.forEach(link => {
-        // Получаем атрибут href ссылки (например, "index.html")
         const linkPath = link.getAttribute('href');
 
-        // 3. Проверяем, совпадает ли адрес ссылки с текущим адресом
-        // Используем .includes(), чтобы корректно работало и с полными путями
         if (currentPath.includes(linkPath)) {
             link.classList.add('active');
         }
 
-        // Обработка главной страницы, если путь пустой или "/"
         if (currentPath === '/' && (linkPath === 'index.html' || linkPath === '/')) {
             link.classList.add('active');
         }
     });
-    console.log("Общая навигация готова");
 }
