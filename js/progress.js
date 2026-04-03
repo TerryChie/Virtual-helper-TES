ab215f9a4252561803cedab6179cc8f28cb3af55
TerryChie <73026499+TerryChie@users.noreply.github.com>
2026-04-03 23:06:50 +0500

    not finished

--- js/progress.js
+++ js/progress.js
@@ -1,17 +1,45 @@
+const path = window.location.pathname;
+const currentLabId = path.split('/').pop().replace('.html', '');
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    const doneLabs = document.getElementById('doneLabs');
-    let doneLabsNumber = parseInt(doneLabs.innerText);
+    const labComplete = document.getElementById('labComplete');
+    labComplete.onclick = completeLab;
 
-    if(Number.isFinite(parseInt(localStorage.getItem('doneLabs')))) {
-        doneLabsNumber = parseInt(localStorage.getItem('doneLabs'));
-        doneLabs.innerText = doneLabsNumber;
+    initializeProgress();
+    console.log(JSON.parse(localStorage.getItem('virtual_lab_progress')));
+});
+
+function initializeProgress() {
+const STORAGE_KEY = 'virtual_lab_progress';
+
+    if (!localStorage.getItem(STORAGE_KEY)) {
+        const initialSkeleton = {
+            "user_info": {
+                "last_activity": new Date().toISOString(),
+                "overall_test_score": 0
+            },
+            "lab1": {
+                "is_completed": false,
+                "test_score": 0,
+                "attempts": 0
+            },
+            "lab2": {
+                "is_completed": false,
+                "test_score": 0,
+                "attempts": 0
+            }
+        };
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(initialSkeleton));
     }
+}
 
-    const clickHere = document.getElementById('plusOne');
-    clickHere.addEventListener('click', () => {
-        doneLabsNumber += 1;
-        doneLabs.innerText = doneLabsNumber;
-        localStorage.setItem('doneLabs', `${doneLabsNumber}`)
-    })
-})
+function getProgress() {
+    const rawData = localStorage.getItem('virtual_lab_progress');
+    return JSON.parse(rawData);
+}
+
+function completeLab() {
+    let currentData = getProgress();
+    const labCompletion = document.getElementById('labCompletion')
+}
